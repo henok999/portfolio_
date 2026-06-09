098f1d629256cbf3fe6c267fe9d2e9d3e33c48b7
henok999 <henokdebebeabera@gmail.com>
2026-06-09 10:03:24 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+window.addEventListener("scroll",()=>{
+
+const navbar=document.querySelector(".navbar");
+
+if(window.scrollY>50){
+navbar.style.background="#041125";
+}
+else{
+navbar.style.background="rgba(0,0,0,.3)";
+}
+
+});
+
+console.log("Henok Portfolio Loaded Successfully");
+
+
+window.addEventListener("scroll",()=>{
+
+const navbar=document.querySelector(".navbar");
+
+if(window.scrollY>50){
+navbar.style.background="#041125";
+}
+else{
+navbar.style.background="rgba(0,0,0,.3)";
+}
+
+});
+
+console.log("Henok Portfolio Loaded Successfully");
